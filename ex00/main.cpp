40816a5c4e88dfd5777bdae6aaf6adc077198ebe
Lea Medrano <leamedrano@Leas-MacBook-Pro.local>
2024-06-20 18:45:03 +0200

    templates swap min and max

---
+++ ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmedrano <lmedrano@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 18:29:32 by lmedrano          #+#    #+#             */
+/*   Updated: 2024/06/20 18:41:06 by lmedrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "swap.hpp"
+#include "max.hpp"
+#include "min.hpp"
+#include <iostream>
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return (0);
+}
