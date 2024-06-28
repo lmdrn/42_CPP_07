97fa604b50ad6382f835538f3f154d6b2cdc7150
lmdrn <leamedrano@gmail.com>
2024-06-28 13:34:44 +0200

    added some more tests

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: lmedrano <lmedrano@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 18:46:05 by lmedrano          #+#    #+#             */
-/*   Updated: 2024/06/20 20:07:10 by lmedrano         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:31:35 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define ITER_HPP
 
 #include <iostream>
+
+#define RESET "\x1b[0m"
+#define GREEN "\x1b[32m"
+#define ORANGE "\x1b[38;5;208m"
+#define RED "\x1b[31m"
+#define PURPLE "\x1b[35m"
 
 template<typename T, typename Fct>
 void iter(T *array, int len, Fct fct)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmedrano <lmedrano@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 18:29:32 by lmedrano          #+#    #+#             */
-/*   Updated: 2024/06/20 20:07:12 by lmedrano         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:33:29 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,26 @@
 
 int main(void)
 {
+	std::cout << " " << std::endl;
+	std::cout << GREEN << "xx----- TEST WITH AN INT ARRAY -----xx" << RESET << std::endl;
+	std::cout << " " << std::endl;
 	int	array[] = { 1, 2, 3, 4, 5 };
 
 	int	arraySize = sizeof(array) / sizeof(array[0]);
 
-	std::cout << "Test 1 : Printing the array" << std::endl;
+	std::cout << GREEN << "Test 1 : Printing the array" << RESET << std::endl;
 	iter(array, arraySize, printElem<int>);
 	std::cout << " " << std::endl;
 
-	std::cout << "Test 2 : Increment by one then print array" << std::endl;
+	std::cout << GREEN << "Test 2 : Increment by one then print array"  << RESET << std::endl;
 	iter(array, arraySize, addOne<int>);
 	std::cout << " " << std::endl;
 
-	std::cout << "Test 3 : Print the string array" << std::endl;
+	std::cout << GREEN << "Test 3 : Print the string array" << RESET << std::endl;
+
+	std::cout << " " << std::endl;
+	std::cout << GREEN << "xx----- TEST WITH A STR ARRAY -----xx" << RESET << std::endl;
+	std::cout << " " << std::endl;
 
 	std::string wunderbar[] = {"I", "am", "wonderful!"};
 	int strArraySize = sizeof(wunderbar) / sizeof(wunderbar[0]);
